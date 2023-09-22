e50a186e704bc534eedd1af8c53ac12d2e7b1156
YourGitHubUsername <muhammedhafeed222@gmail.com>
2023-09-22 22:25:05 +0530

    two commit

--- About.js
+++ About.js
@@ -3,12 +3,25 @@
 //     button.style.color = "red";
 // }
 
-let button = document.getElementById("button")
-button.onclick = function(){
-    button.style.color = "yellow"
-}
+// let button = document.getElementById("button")
+// button.onclick = function(){
+//     button.style.color = "yellow"
+// }
 
-function changeColor(){
-    const loop = document.getElementById("loop");
-    loop.style.cssText = "color:red;";
+// function changeColor(){
+//     const loop = document.getElementById("loop");
+//     loop.style.cssText = "color:red;";
+// }
+
+  
+ var button = document.getElementById("button")
+ button.onmouseover = function(){
+     button.style.color = "red";
+ }
+ 
+
+//  onload//
+window.onload = function(){
+    let button = document.getElementById("button");
+    button.style.color = "red";
 }
